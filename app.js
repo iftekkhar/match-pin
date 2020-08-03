f6f9427261f41aa4f108c8700cacf8bad783d80a
Iftekhar Anam <iftekhar_anam@hotmail.com>
2020-08-04 04:20:26 +0600

    time

--- app.js
+++ app.js
@@ -1,12 +1,25 @@
 function pinGenerator() {
     // const pin = 2000;
-    const pin = Math.floor(Math.random() * (9999 - 1000 + 1)) + 1000;
+    const pin = Math.floor(Math.random() * (0 - 9 + 1)) + 9;
     document.getElementById('generated-pin-value').value = pin;
 }
 
-function pinMatch(value) {
+const numbers = document.getElementsByClassName('number-buttons');
+for (let i = 0; i < numbers.length; i++) {
+    numbers[i].addEventListener('click', function () {
+        let values = numbers[i].innerText;
+        document.getElementById('typed-input-value').value = values;
+    })
+}
+
+function numberInput(x) {
+    
+}
+
+function pinMatch() {
     const pin = document.getElementById('generated-pin-value').value;
-    if (value == pin) 
+    const inputValue = document.getElementById('typed-input-value').value;
+    if (inputValue == pin) 
         document.getElementById('success').style.display = "block";
     else document.getElementById('failed').style.display = "block";
 }
