4eda28b246a56f81e43c85c217b9afe318375236
Iftekhar Anam <iftekhar_anam@hotmail.com>
2020-08-04 06:51:52 +0600

    full done

--- app.js
+++ app.js
@@ -1,25 +1,71 @@
+// Random Pin Number Generator
 function pinGenerator() {
-    // const pin = 2000;
-    const pin = Math.floor(Math.random() * (0 - 9 + 1)) + 9;
-    document.getElementById('generated-pin-value').value = pin;
+  const initialValue = document.getElementById("generated-pin-value");
+  const pin = Math.floor(Math.random() * (9999 - 1000 + 1)) + 1000;
+  initialValue.value = pin;
+  document.getElementById("trial-left").innerText = 3;
 }
 
-const numbers = document.getElementsByClassName('number-buttons');
+// Input Number Button Function
+const numbers = document.getElementsByClassName("number-buttons");
 for (let i = 0; i < numbers.length; i++) {
-    numbers[i].addEventListener('click', function () {
-        let values = numbers[i].innerText;
-        document.getElementById('typed-input-value').value = values;
-    })
+  numbers[i].addEventListener("click", function () {
+    var output = document.getElementById("typed-input-value").value;
+    let values = numbers[i].innerText;
+    if (output != NaN) {
+      //if output is a number
+      output = output + values;
+      document.getElementById("typed-input-value").value = output;
+    }
+  });
 }
 
-function numberInput(x) {
-    
+// Clear Button
+function clearField() {
+  document.getElementById("typed-input-value").value = "";
 }
 
+//Backspace Button
+function backspaceButton() {
+  var output = document.getElementById("typed-input-value").value.toString();
+  if (output) {
+    //if output has a value
+    output = output.substr(0, output.length - 1);
+    document.getElementById("typed-input-value").value = output;
+  }
+}
+
+// Pin Matcher
 function pinMatch() {
-    const pin = document.getElementById('generated-pin-value').value;
-    const inputValue = document.getElementById('typed-input-value').value;
-    if (inputValue == pin) 
-        document.getElementById('success').style.display = "block";
-    else document.getElementById('failed').style.display = "block";
+  //Pin Compare
+  const pin = document.getElementById("generated-pin-value").value;
+  const inputValue = document.getElementById("typed-input-value").value;
+
+  //Success/Fail Notification
+  if (inputValue == pin) {
+    document.getElementById("success").style.display = "block";
+    setTimeout(function () {
+      document.getElementById("success").style.display = "none";
+    }, 1000);
+    document.getElementById("generated-pin-value").value = "";
+  } else {
+    document.getElementById("failed").style.display = "block";
+    setTimeout(function () {
+      document.getElementById("failed").style.display = "none";
+    }, 1000);
+
+    // Trail Number Updater
+    const numberofTrials = document.getElementById("trial-left").innerText;
+    const trialsLeft = numberofTrials - 1;
+    document.getElementById("trial-left").innerText = trialsLeft;
+
+    // Disbale Submit Button
+    if (trialsLeft == 0) {
+      document.getElementById("submit-button").disabled = true;
+      document.getElementById("random-pin-generator").disabled = true;
+      document.getElementById("submit-button").style.backgroundColor = "#9e9e9e";
+      document.getElementById("random-pin-generator").style.backgroundColor = "#9e9e9e";
+    }
+  }
+  clearField();
 }
